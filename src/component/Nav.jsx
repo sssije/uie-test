89aa48983627ca19cc4763523ba9f548b8c40498
sssije <87226364+sssije@users.noreply.github.com>
2021-07-12 15:29:33 +0700

    create header (#1)
    Co-authored-by: Siti Jumaliaya <siti.jumaliaya@bukalapak.com>

---
+++ src/component/Nav.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import './Nav.scss';
+import '../index.scss';
+
+const navbarList = [
+  {
+    title: 'Life',
+    url: '/',
+  }, {
+    title: 'Jobs',
+    url: '/',
+  }, {
+    title: 'Tech',
+    url: '/',
+  }, {
+    title: 'Event',
+    url: '/',
+  }
+];
+
+const list = navbarList.map(item => 
+  <li className="c-navbar-item">
+    <a className="navbar-item__link" href={item.url} target="_blank">{item.title}</a>
+  </li>
+);
+
+class Nav extends React.Component {
+  render () {
+    return (
+      <nav className="c-navbar">
+        <ul className="c-navbar-list">{list}</ul>
+      </nav>
+    )
+  };
+};
+
+export default Nav;
---
+++ src/pages/Landing.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '../component/Header.jsx';
+
+function Landing() {
+  return (
+    <div className="main">
+      <Header />
+    </div>
+  );
+};
+
+export default Landing;
